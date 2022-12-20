b3435995457f734b147c2d5eb6b3c13b7360c389
skolim10 <102150196+skolim10@users.noreply.github.com>
2022-12-20 16:54:19 +0000

    Deploying to gh-pages from @ skolim10/goit-react-hw-05-movies@a54f892ffe683edc4e5dfe6513f32a55891f6c6b 🚀

---
+++ static/js/256.a5c6c9cc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:54},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that movie :("})]})}}}]);
+//# sourceMappingURL=256.a5c6c9cc.chunk.js.map
