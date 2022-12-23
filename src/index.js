b45537af53029533e7b91b528927631deb104367
Tomasz Skolimo <skolim10@gmail.com>
2022-12-24 00:52:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basname={/goit-react-hw-05-movies/}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
